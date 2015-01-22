f823e1f7aec5e6b45f692b76793bfea08ecbbfb9
mcgavin <aj94450@ens.etsmtl.ca>
2015-01-22 09:42:49 -0500

    par test

--- par/tp1.c
+++ par/tp1.c
@@ -2,8 +2,18 @@
 #include "mpi.h"
 
 void main (int argc, char *argv[]){
-	int err;
-	err = MPI_Init(&argc, &argv);
-	printf("Hello world!\n" );
-	err = MPI_Finalize();
+	int myid, numprocs;
+    FILE *f1;
+    int i;
+ 
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+ 
+
+        /* print out my rank and this run's PE size*/
+    printf("Hello from %d\n",myid);
+    printf("Numprocs is %d\n",numprocs);
+
+    MPI_Finalize();
 }
