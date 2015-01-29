fb4ad3ec7934be27ac877aa729cf1554f75b6d2c
Valentine Berge <berge.valentine@gmail.com>
2015-01-29 10:15:41 -0500

    parallele ok

--- par/tp1.c
+++ par/tp1.c
@@ -14,7 +14,7 @@ char *argv[];
     int myid, numprocs;
     int tag,source,destination,count;
     int buffer;
-    int p, n, x, i;
+    int p, n, x, i, j;
     MPI_Status status;
  
     MPI_Init(&argc,&argv);
@@ -35,21 +35,27 @@ char *argv[];
         for (x = 0; x <= n; x++)
         {
             for (i = 0; i < 8; i++) {
-                //usleep(1000);
+                usleep(1000);
                 array[i] = array[i] + (i + myid)*x;
             }
         }
 
-        int send=array[0];
-
-        MPI_Send(array,count,MPI_INT,8,tag,MPI_COMM_WORLD);
-        printf("processor %d  sent %d\n",myid,send);
+        MPI_Send(&array,8,MPI_INT,8,tag,MPI_COMM_WORLD);
+        //printf("processor %d  sent %d\n",myid,array[0]);
     }
     if(myid == 8){
-        //int matrix[8][8];
+        int matrix[8][8];
         for (i = 0; i < 8; i++){
-            MPI_Recv(&buffer,count,MPI_INT,i,tag,MPI_COMM_WORLD,&status);
-            printf("processor %d  got %d\n",myid,buffer);
+            MPI_Recv(matrix[i],8,MPI_INT,i,tag,MPI_COMM_WORLD,&status);
+            //printf("processor %d  got %d\n",myid,matrix[i][0]);
+        }
+
+        for (i = 0; i < 8; i++) {
+            for (j = 0; j < 8; j++) {
+                printf("%d", matrix[i][j]);
+                printf("    ");
+            }
+            printf("\n");
         }
 
     }
