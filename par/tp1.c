bf23c48f00889818383963bfebc9460b33f5fef5
Valentine Berge <berge.valentine@gmail.com>
2015-01-29 10:02:32 -0500

    version parallele qui marche

--- par/tp1.c
+++ par/tp1.c
@@ -12,75 +12,46 @@ int argc;
 char *argv[];
 {
     int myid, numprocs;
-    int tag,count, i;
+    int tag,source,destination,count;
     int buffer;
+    int p, n, x, i;
     MPI_Status status;
  
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
     tag=1234;
+    source=0;
+    destination=1;
     count=1;
+    p=3;
+    n=5;
 
     if(myid < 8){
-        int x;
+        //int *array = (int*)malloc(8*sizeof(int));
         int array[8];
-        for(i = 0; i < 8; i++) {array[i] = p;}
-        
-        for (x = 0; x <= var_k; x++){
+        for (i = 0; i < 8; i++) array[i] = p;
+
+        for (x = 0; x <= n; x++)
+        {
             for (i = 0; i < 8; i++) {
-                usleep(1000);
+                //usleep(1000);
                 array[i] = array[i] + (i + myid)*x;
             }
         }
 
-        buffer=array[7];
-        MPI_Send(&buffer,count,MPI_INT,8,tag,MPI_COMM_WORLD);
-        printf("processor %d  sent %d\n",myid,buffer);
+        int send=array[0];
+
+        MPI_Send(array,count,MPI_INT,8,tag,MPI_COMM_WORLD);
+        printf("processor %d  sent %d\n",myid,send);
     }
-
     if(myid == 8){
-        int matrix[8][8], j;
-        
-        MPI_Recv(&buffer,count,MPI_INT,0,tag,MPI_COMM_WORLD,&status);
-        printf("processor %d  got %d\n",myid,buffer);
-        matrix[7][0]=buffer;
-
-        MPI_Recv(&buffer,count,MPI_INT,1,tag,MPI_COMM_WORLD,&status);
-        printf("processor %d  got %d\n",myid,buffer);
-        matrix[7][1]=buffer;
-
-        MPI_Recv(&buffer,count,MPI_INT,2,tag,MPI_COMM_WORLD,&status);
-        printf("processor %d  got %d\n",myid,buffer);
-        matrix[7][2]=buffer;
-
-        MPI_Recv(&buffer,count,MPI_INT,3,tag,MPI_COMM_WORLD,&status);
-        printf("processor %d  got %d\n",myid,buffer);
-        matrix[7][3]=buffer;
-
-        MPI_Recv(&buffer,count,MPI_INT,4,tag,MPI_COMM_WORLD,&status);
-        printf("processor %d  got %d\n",myid,buffer);
-        matrix[7][4]=buffer;
-
-        MPI_Recv(&buffer,count,MPI_INT,5,tag,MPI_COMM_WORLD,&status);
-        printf("processor %d  got %d\n",myid,buffer);
-        matrix[7][5]=buffer;
-
-        MPI_Recv(&buffer,count,MPI_INT,6,tag,MPI_COMM_WORLD,&status);
-        printf("processor %d  got %d\n",myid,buffer);
-        matrix[7][6]=buffer;
-
-        MPI_Recv(&buffer,count,MPI_INT,7,tag,MPI_COMM_WORLD,&status);
-        printf("processor %d  got %d\n",myid,buffer);
-        matrix[7][7]=buffer;
-
-        for (i = 0; i < 8; i++) {
-            for (j = 0; j < 8; j++) {
-                printf("%d", maxtrix[i][j]);
-                printf("    ");
-            }
-            printf("\n");
+        //int matrix[8][8];
+        for (i = 0; i < 8; i++){
+            MPI_Recv(&buffer,count,MPI_INT,i,tag,MPI_COMM_WORLD,&status);
+            printf("processor %d  got %d\n",myid,buffer);
         }
+
     }
     MPI_Finalize();
 }
